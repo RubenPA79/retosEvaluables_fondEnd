6d17c35ea0dc2e197690bfe0cdf2e995ecfd4323
RubenPA79 <rpaxonarias79@gmail.com>
2025-02-06 10:13:08 +0100

    commit

---
+++ modulo3-Tema1/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './page/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
